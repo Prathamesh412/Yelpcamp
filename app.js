fcce83838bb6e3f96c2ba69ba8764a97c2a38541
Prathamesh412 <prathprabhu@gmail.com>
2018-09-11 00:54:59 +0530

    Changes made in app.js file to run on heroku

--- app.js
+++ app.js
@@ -72,4 +72,4 @@ app.use(campgrondRoutes);
 //Routes
 
 
-app.listen(3000, () => console.log('App listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('App listening on port 3000!'));
